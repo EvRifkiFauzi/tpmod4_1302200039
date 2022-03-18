7aa99b473021f7c4c77dbb15168ad955abe04679
EvRifkiFauzi <100852580+EvRifkiFauzi@users.noreply.github.com>
2022-03-18 17:37:27 +0700

    generic
    generic

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmod4_1302200039
+{
+    public class ClassMain
+    {
+        public static void Main(string[] args)
+        {
+            String nama = "Rifki";
+            HaloGeneric.SapaUser<string>(ref nama);
+            DataGeneric<string> dataGeneric = new DataGeneric<string>("1302200039");
+            dataGeneric.PrintData();
+        }
+    }
+
+    class HaloGeneric
+    {
+        public static void SapaUser<T>(ref T nama)
+        {
+            Console.WriteLine("Halo user " + nama);
+        }
+    }
+
+    public class DataGeneric<T>
+    {
+        private T Data;
+
+        public DataGeneric(T Data)
+        {
+            this.Data = Data;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine("Data yang tersimpan adalah " + Data);
+        }
+    }
+}
